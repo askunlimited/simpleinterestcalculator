eed9ba4d839abf8eb907c1f7e14501ea0e6b0e75
silentcoder <ask.sunn@live.com>
2021-04-16 15:55:16 +0100

    Edited code with all corrections

--- script.js
+++ script.js
@@ -4,23 +4,20 @@ function updateRate() {
     document.getElementById("rate_val").innerText = rateval;
 }
 
-function compute()
-{
-    
+function compute() {
+
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-var interest = (principal * years * rate) /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    
+    var interest = (principal * years * rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+
     if (principal < 1) {
         alert("enter a positive number");
         document.getElementById("principal").focus();
     } else {
-        document.getElementById("result").innerHTML = "if you deposit: " + principal + "\<br\>" + 
-"at an interest rate of: " + rate + "%" "\<br\>"  +
-"You will receive an interest of " + interest + "\<br\>"  +
-"at the end of " + year
-    
+        document.getElementById("result").innerHTML = "if you deposit: <mark>" + principal + "</mark>, \<br\>" + "at an interest rate of: <mark>" + rate + "% </mark>," + "\<br\>" +
+            "You will receive an interest of <mark>" + interest + "</mark>, \<br\>" +
+            "at the end of <mark>" + year + "</mark>"
     }
-        
+};
