1496fcbb553167ad6f6c69dd3892a8896aae9260
askunlimited <ask.sunn@live.com>
2021-04-16 15:17:44 +0100

    made changes to the script due to the feedback

--- script.js
+++ script.js
@@ -6,17 +6,21 @@ function updateRate() {
 
 function compute()
 {
+    
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 var interest = (principal * years * rate) /100;
     var year = new Date().getFullYear()+parseInt(years);
-
-document.getElementById("result").innerHTML = "if you deposit: " + principal + '\n' + 
-"at an interest rate of: " + rate + '\n' +
-"You will receive an interest of " + interest + '\n' +
-"At the end of " + year
     
+    if (principal < 1) {
+        alert("enter a positive number");
+        document.getElementById("principal").focus();
+    } else {
+        document.getElementById("result").innerHTML = "if you deposit: " + principal + "\<br\>" + 
+"at an interest rate of: " + rate + "%" "\<br\>"  +
+"You will receive an interest of " + interest + "\<br\>"  +
+"at the end of " + year
     
-}
+    }
         
